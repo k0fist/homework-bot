e5740f3d2029c6c4d14bee1d950c0dea3c741dbe
Vadim Sirotkin <sirotkin.201515@gmail.com>
2025-03-06 16:05:35 +0500

    Edits before review

---
+++ exceptions.py
@@ -0,0 +1,16 @@
+class NotFoundTokenException(Exception):
+    """Исключение пустоты необходимых токенов."""
+
+    pass
+
+
+class StatusHomeworkException(Exception):
+    """Исключение ответа от Практикум Домашка."""
+
+    pass
+
+
+class NotFoundHomeworkNameException(Exception):
+    """Исключение ответа от Практикум Домашка."""
+
+    pass
--- homework.py
+++ homework.py
@@ -1,16 +1,26 @@
-...
+import logging
+import os
+import requests
+import time
+import json
+
+from dotenv import load_dotenv
+from telebot import TeleBot
+from exceptions import (
+    NotFoundTokenException,
+    StatusHomeworkException,
+    NotFoundHomeworkNameException
+)
 
 load_dotenv()
 
-
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-
 
 HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
@@ -18,49 +28,124 @@ HOMEWORK_VERDICTS = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    filename='main.log',
+    level=logging.DEBUG,
+    filemode='w',
+    encoding='utf-8'
+)
+
 
 def check_tokens():
-    ...
+    """Проверка, что переменных окружения не пустые."""
+    tokens = (
+        ('PRACTICUM_TOKEN', PRACTICUM_TOKEN),
+        ('TELEGRAM_TOKEN', TELEGRAM_TOKEN),
+        ('TELEGRAM_CHAT_ID', TELEGRAM_CHAT_ID),
+    )
+    missing_tokens = []
+    for name, value in tokens:
+        if value is None or value == '':
+            missing_tokens.append(name)
+    if missing_tokens:
+        logging.critical(
+            f'Нужные переменные окружения пустые: {", ".join(missing_tokens)}'
+        )
+        raise NotFoundTokenException('Нужные токены пустые!')
+    else:
+        logging.debug('Все токены найдены')
 
 
 def send_message(bot, message):
-    ...
+    """Отправка ботом сообщения."""
+    try:
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+        logging.debug('Сообщение отправлено пользователю')
+    except Exception as error:
+        logging.error(
+            'Сообщение не отправлено пользователю '
+            f'Произошла ошибка: {error}. '
+        )
 
 
 def get_api_answer(timestamp):
-    ...
+    """Получение ответа от сервиса Практикум Домашка по API."""
+    try:
+        homework_statuses = requests.get(
+            ENDPOINT,
+            headers=HEADERS,
+            params={'from_date': timestamp}
+        )
+        status_code = homework_statuses.status_code
+        if status_code != 200:
+            raise Exception(f'API возвращает код {status_code}')
+        data = homework_statuses.json()
+        check_response(data)
+        homeworks = data.get('homeworks', [])
+        if not homeworks:
+            return {}
+        return homeworks[0]
+    except requests.RequestException as error:
+        logging.error(f'Ошибка, Практикум Домашка недоступна: {error}')
+    except json.decoder.JSONDecodeError as error:
+        logging.error(f'Ошибка, ответ пришел не в json формате: {error}')
+    return None
 
 
 def check_response(response):
-    ...
+    """Проверка ответа от сервиса Практикум Домашка по API."""
+    if not isinstance(response, dict):
+        raise TypeError("Ожидается словарь в ответе.")
+    homeworks = response.get('homeworks', None)
+    if not isinstance(homeworks, list):
+        raise TypeError("Ожидается список под ключом 'homeworks'.")
+    if 'homeworks' not in response:
+        raise KeyError("Отсутствует ключ 'homeworks'.")
+    if homeworks == []:
+        logging.debug('Проектов за этот период нет')
+        return False
+    return True
 
 
 def parse_status(homework):
-    ...
-
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    """Парсинг ответа от Практикум."""
+    homework_name = 'Домашка'
+    homework_name = homework.get('homework_name')
+    if homework_name is None:
+        raise NotFoundHomeworkNameException("Не было найдено название работы")
+    status = homework.get('status')
+    if status is None or HOMEWORK_VERDICTS.get(status) is None:
+        raise StatusHomeworkException(
+            "Не был найден статус работы или не существующе название"
+        )
+    return (
+        f'Изменился статус проверки работы "{homework_name}". '
+        f'{HOMEWORK_VERDICTS.get(status)}'
+    )
 
 
 def main():
     """Основная логика работы бота."""
+    check_tokens()
 
-    ...
-
-    # Создаем объект класса бота
-    bot = ...
+    bot = TeleBot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
-
-    ...
-
     while True:
         try:
-
-            ...
-
+            response = get_api_answer(timestamp)
+            if not response:
+                logging.debug('Нет домашних работ.')
+            else:
+                status = parse_status(get_api_answer(timestamp))
+                if status is not None:
+                    send_message(bot, status)
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
-        ...
+            logging.error(message)
+        except StatusHomeworkException as error:
+            logging.error(f'Ошибка {error}')
+        time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
